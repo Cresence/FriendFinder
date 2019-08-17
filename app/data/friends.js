f057bc784e5791f92ce18a4aec11d3a47ea03a5d
Cresence <devharris93@gmail.com>
2019-08-17 15:39:49 -0400

    Added base files

---
+++ app/data/friends.js
@@ -0,0 +1,16 @@
+let friends = [
+    {
+        friendName: "Gatts",
+        trait: "Loyal"
+    },
+    {
+        friendName: "Griffith",
+        trait: "Leader"
+    },
+    {
+        friendName: "Casca",
+        trait: "Loving"
+    }
+]
+
+module.exports = { data: friends };
---
+++ server.js
@@ -0,0 +1,18 @@
+// Dependencies
+// =============================================================
+const express = require("express");
+const path = require("path");
+const data = require("./app/data/friends.js")
+
+// Sets up the Express App
+// =============================================================
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
