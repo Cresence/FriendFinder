e1de999d35e8ed76f191ef0655c53ef417099a4a
Cresence <devharris93@gmail.com>
2019-08-25 13:48:57 -0400

    Tried to make the images appear, no luck

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,17 +2,17 @@ let friends = [
     {
         friendName: "Gatts",
         trait: "Loyal",
-        photo: "https://vignette.wikia.nocookie.net/berserk/images/4/40/Manga_V38_Guts.png/revision/latest/scale-to-width-down/350?cb=20170919104357"
+        photo: "./assets/guts.png"
     },
     {
         friendName: "Griffith",
         trait: "Leader",
-        photo: "https://vignette.wikia.nocookie.net/berserk/images/b/b0/BTCG_Griffith_Holding_Sabre.png/revision/latest?cb=20170930052614"
+        photo: "./assets/griffith.png"
     },
     {
         friendName: "Casca",
         trait: "Loving",
-        photo: "https://vignette.wikia.nocookie.net/berserk/images/d/d0/BTCG_Casca_and_Puck.png/revision/latest?cb=20170923104942"
+        photo: "./assets/casca.png"
     }
 ]
 
